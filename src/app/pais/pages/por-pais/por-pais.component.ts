338e24aa53ec0f8a7731ca350d99df9b27578cbd
Laura Juiz <ljuiz@southernhorizon.tech>
2021-11-03 14:35:41 -0300

    Creación por país

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styleUrls: ['./por-pais.component.css']
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+
+  termino: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  buscar() {
+    if (this.termino.trim().length === 0) {
+      return
+    }
+    console.log(this.termino);
+    this.termino = '';
   }
 
 }
