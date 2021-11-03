e5eba752d2d3ba1c79091c70298af4b548150311
Laura Juiz <ljuiz@southernhorizon.tech>
2021-11-03 13:52:50 -0300

    Componente sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
-
-
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class SharedModule { }
